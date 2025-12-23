075460dafa80e287084856c86650ef9eb764a6a8
Pawas Ranjan <pawasranjan5@gmail.com>
2025-12-23 19:57:39 +0530

    Create a C# program that validates candy flavours and calculates discounted prices using classes and objects.

---
+++ Assignment/CandyCraze/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace CandyCraze;
+
+public class Program
+{
+    public static void Main()
+    {
+        Candy candy = new Candy();
+
+        Console.WriteLine("Enter the flavour ");
+        candy.Flavour = Console.ReadLine();
+
+        Console.WriteLine("Enter the Quantity ");
+        candy.Quantity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the price per piece ");
+        candy.PricePerPiece = int.Parse(Console.ReadLine());
+
+        if (candy.ValidateCandyFlavour())
+        {
+            Candy result = CalculateDiscountedPrice(candy);
+
+            Console.Write("Flavour: " + result.Flavour);
+            Console.Write("\nQuantity : " + result.Quantity);
+            Console.Write("\nPrice Per Piece : " + result.PricePerPiece);
+            Console.Write("\nTotal Price: " + result.TotalPrice);
+            Console.Write("\nDiscount Price : " + result.Discount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid flavour");
+        }
+    }
+
+    public static Candy CalculateDiscountedPrice(Candy candy)
+    {
+        double DiscountPer = 0;
+
+        if(candy.Flavour == "Strawberry")
+        {
+            DiscountPer = 15;
+        }
+        else if(candy.Flavour == "Lemon")
+        {
+            DiscountPer = 10;
+        }
+        else if(candy.Flavour == "Mint")
+        {
+            DiscountPer = 5;
+        }
+        
+        candy.TotalPrice = candy.Quantity * candy.PricePerPiece;
+        candy.Discount = candy.TotalPrice - (candy.TotalPrice * DiscountPer / 100);
+
+        return candy;
+    }
+}
