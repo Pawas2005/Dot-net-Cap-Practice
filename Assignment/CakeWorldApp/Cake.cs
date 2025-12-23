372c1943a190ee6293ac4aacc816f1ee4becda25
Pawas Ranjan <pawasranjan5@gmail.com>
2025-12-23 16:24:56 +0530

    Create a C# program to demonstrate custom and user-defined exceptions by validating cake orders and calculating discounted prices based on cake flavour and quantity.

--- Assignment/CakeWorldApp/Cake.cs
+++ Assignment/CakeWorldApp/Cake.cs
@@ -1,4 +1,65 @@
 using System;
 
-using Cake;
+namespace CakeWorld;
 
+public class Cake
+{
+    public string Flavour{ get; set; }
+    public int QuantityInKg{ get; set; }
+    public double PricePerKg{ get; set; }
+
+    public bool CakeOrder()
+    {
+        if(!(Flavour == "Chocolate" || Flavour == "Red Velvet" || Flavour == "Vanilla"))
+        {
+            throw new InvalidFlavourException(
+                "Flavour not available. Please select the available flavour."
+            );
+        }
+
+        if (QuantityInKg <= 0)
+        {
+            throw new NegativeQuantityInKgException("Quantity must be greater than zero");
+        }
+        return true;
+    }
+
+    public double CalculatePrice()
+    {
+        double totalPrice = QuantityInKg * PricePerKg;
+        double discount = 0;
+
+        if(Flavour == "Vanilla")
+        {
+            discount = 3;
+        }
+        else if(Flavour == "Chocolate")
+        {
+            discount = 5;
+        }
+        else if(Flavour == "Red Velvet")
+        {
+            discount = 10;
+        }
+
+        double discountedPrice = totalPrice - (totalPrice * discount / 100);
+
+        return discountedPrice;
+    }
+
+    public class InvalidFlavourException : Exception
+    {
+        public InvalidFlavourException(string message) : base(message)
+        {
+            
+        }
+    }
+
+    public class NegativeQuantityInKgException : Exception
+    {
+        public NegativeQuantityInKgException(string message) : base(message)
+        {
+            
+        }
+    }
+}
--- Assignment/CakeWorldApp/Program.cs
+++ Assignment/CakeWorldApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CakeWorld;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cake = new Cake();
+
+        Console.WriteLine("Enter the flavour ");
+        cake.Flavour = Console.ReadLine();
+
+        Console.WriteLine("Enter the quantity in kg ");
+        cake.QuantityInKg = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the price per kg ");
+        cake.PricePerKg = int.Parse(Console.ReadLine());
+
+        try
+        {
+            cake.CakeOrder();
+            Console.WriteLine("Cake order was successful");
+            Console.WriteLine("Price after discount is " + cake.CalculatePrice());
+        }
+        catch (Cake.InvalidFlavourException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Cake.NegativeQuantityInKgException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
