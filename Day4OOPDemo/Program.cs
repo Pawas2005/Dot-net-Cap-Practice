a182c98176cf1685da75b74b4ea572b83a3b0e60
Pawas Ranjan <pawasranjan5@gmail.com>
2025-12-26 21:34:08 +0530

    little bit about returning method with or without parameters.

--- Day4OOPDemo/Program.cs
+++ Day4OOPDemo/Program.cs
@@ -2,14 +2,68 @@
 
 namespace OOPS
 {
-    class Program
+    // class Program
+    // {
+    //     static void Main()
+    //     {
+    //         UserDefinedTypes udt = new UserDefinedTypes();
+    //         Emp obj = udt.GetEmpDetails(1001);
+    //         Console.WriteLine(obj.id + " " + obj.name + " " + obj.job + " " + obj.sal + " " + obj.status);
+    //         Console.ReadLine();
+    //     }
+    // }
+
+    internal class Program
     {
+        //Non-value returning method without parameters  
+        public void Test1() //Static in behavior   
+        {
+            int x = 5;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{x} * {i} = {x * i}");
+            }
+        }
+
+        //Non-value returning method with parameters 
+        public void Test2(int x, int ub) //Dynamic in behavior       
+        {
+            for (int i = 1; i <= ub; i++)
+            {
+                Console.WriteLine($"{x} * {i} = {x * i}");
+            }
+        }
+
+        //Value returning method without parameters 
+        public string Test3() //Static in behavior   
+        {
+            string str = "hello world";
+            str = str.ToUpper();
+            return str;
+        }
+
+        //Value returning method with parameters 
+        public string Test4(string str) //Dynamic in behavior   
+        {
+            str = str.ToUpper();
+            return str;
+        }
+
         static void Main()
         {
-            UserDefinedTypes udt = new UserDefinedTypes();
-            Emp obj = udt.GetEmpDetails(1001);
-            Console.WriteLine(obj.id + " " + obj.name + " " + obj.job + " " + obj.sal + " " + obj.status);
-            Console.ReadLine();
+            Program p = new Program();
+            
+            //Calling non-value returning methods. 
+            p.Test1();
+            Console.WriteLine();
+            p.Test2(8, 15);
+            Console.WriteLine();
+
+            //Calling value returning methods 
+            string s1 = p.Test3();
+            Console.WriteLine(s1);
+            string s2 = p.Test4("hello india");
+            Console.WriteLine(s2);
         }
     }
 }
