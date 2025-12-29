b1aa51c78a2aa7ac021b9202baef1e27e87776f5
Pawas Ranjan <pawasranjan5@gmail.com>
2025-12-29 21:33:04 +0530

    Add LPUMgmtSystem

---
+++ LPUMgmtSystem/LPU_DAL/StudentDAO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using LPU_Common;
+using LPU_Entity;
+using LPU_Exceptions;
+
+namespace LPU_DAL
+{
+    /// <summary>
+    /// Student DAO class is used for CRUD Operations
+    /// </summary>
+    public class StudentDAO : IStudentCRUD
+    {
+        static List<Student> studentList = null;
+        public StudentDAO()
+        {
+            //Collection Init
+            studentList = new List<Student>()
+            {
+                new Student() { StudentID = 101, Name = "Pawas", Course = CourseType.CSE, Address = "Chandigarh" },
+                new Student() { StudentID = 102, Name = "Alok", Course = CourseType.CSE, Address = "Jalandhar" },
+                new Student() { StudentID = 103, Name = "Alia", Course = CourseType.CSE, Address = "Amritsar" },
+                new Student() { StudentID = 104, Name = "Rajat", Course = CourseType.CSE, Address = "Phagwara" },
+                new Student() { StudentID = 105, Name = "Aman", Course = CourseType.CSE, Address = "Patiala" },
+            };
+        }
+        public bool DropStudentDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EnrollStudent(Student sObj)
+        {
+            bool flag = false;
+            if(sObj != null)
+            {
+                studentList.Add(sObj);
+                flag = true;
+            }
+            return flag;
+        }
+
+        public Student SearchStudentByID(int rollNo)
+        {
+            Student myStud = null;
+            if(rollNo != 0)
+            {
+                myStud = studentList.Find(s => s.StudentID == rollNo);
+                if(myStud == null)
+                {
+                    throw new LPUException("Student Record Not Found.");
+                }
+            }
+            else
+            {
+                throw new LPUException("Error Generated");
+            }
+            return myStud;
+        }
+
+        public List<Student> SearchStudentByName(string name)
+        {
+            List<Student> data = studentList.FindAll(p => p.Name == name);
+            return data;
+        }
+
+        public bool UpdateStudentDetails(int id, Student newObj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LPUMgmtSystem/LPU_Exceptions/LPUException.cs
@@ -0,0 +1,18 @@
+﻿namespace LPU_Exceptions
+{
+    ///<summary>
+    ///Custom Exception Class Created for LPU Project
+    ///By Pawas on Date 29/12/2025 at 11:35AM
+    ///</summary>
+    public class LPUException : Exception
+    {
+        public LPUException() : base()
+        {
+
+        }
+        public LPUException(string errorMsg) : base(errorMsg)
+        {
+
+        }
+    }
+}
